72ebf39424e3e618201bcbb44cb0b062e9e4b968
Davide Baldelli <davbaldelli@gmail.com>
2021-10-25 17:31:02 +0200

    added file selector

--- src/pages/Page2.js
+++ src/pages/Page2.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, {useRef} from "react";
 import qr from '../immagini/qrpng.png';
 import camera from "../immagini/Camera.png";
 import image from "../immagini/Image.png";
 import avanti from "../immagini/avanti.png";
 import {Link} from 'react-router-dom';
 import './Pages.css';
-
+import jsQR from "jsqr";
+import {get} from '@andreekeberg/imagedata'
 
 import {Switch,Route,Redirect, withRouter} from 'react-router-dom';
 
 function Page2() {
+
+    const inputFile = useRef(null)
+
+    function onImageSelectorClick(){
+        inputFile.current.click();
+    }
+
+    function notifyQrCodeFound(){}
+
+    function notifyQrCodeNotFound(){}
+
+    function onImageInput(file){
+        get(file, (error, imageData) => {
+            if (error) {
+                console.log(error)
+            } else {
+                let code = jsQR(imageData.data, imageData.width, imageData.height).data;
+                if (code) {
+                    notifyQrCodeFound()
+                } else {
+                    notifyQrCodeNotFound()
+                }
+            }
+        })
+    }
+
     return (
         <div>
             <div>
@@ -28,19 +55,20 @@ function Page2() {
                 <div className="loadOptions">
                     <div className="loadsx">
                         <div>
-                            <img src={camera} className="icon" />
+                            <img src={camera} className="icon" alt={"camera icon"}/>
                         </div>
                         <div className="loadLabel">
                             Fotocamera
                         </div>
                     </div>
-                    <div className="loaddx">
+                    <div className="loaddx" onClick={onImageSelectorClick}>
                         <div>
-                            <img src={image} className="icon" />
+                            <img src={image} className="icon" alt={"gallery icon"}/>
                         </div>
                         <div className="loadLabel">
                             Galleria
                         </div>
+                        <input type="file" ref={inputFile} accept="image/png, image/jpeg" onChange={ e => (onImageInput(e.target.files[0])) } style={{display: 'none'}}/>
                     </div>
                 </div>
                 <footer>
